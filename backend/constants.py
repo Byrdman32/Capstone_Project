eb3d53097a572376029932b2ea13e9662cb0ca4a
copper-head <duncan@truitthome.com>
2025-04-22 20:32:12 -0500

    Modified docker compose file to create posgres database service. Also added a schema file and modified the docker file to run the schema on the database on creation of the container. If you run into an issue with the database showing the expected tables recreate the container. Also added a constants file and updated the main file to reflect changes.

---
+++ backend/constants.py
@@ -0,0 +1,9 @@
+
+
+# In general if this is a container keep the address to '0.0.0.0' to allow access from outside the container.
+# If you want to test locally set it to '127.0.0.1' or 'localhost'.
+API_ADDRESS = '0.0.0.0'
+
+# MAKE SURE THAT IF YOU CHANGE THIS PORT THAT THE PORT IS FORWARDED IN DOCKER
+# Check docker-compose.yml to see this.
+API_PORT = 9000
