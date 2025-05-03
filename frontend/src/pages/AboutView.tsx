4fb7afa094751a1665f61d54f33f31f383a643a9
Viktor-Butkovich <51521287+Viktor-Butkovich@users.noreply.github.com>
2025-05-03 16:20:11 -0500

    Split page files

---
+++ frontend/src/pages/AboutView.tsx
@@ -0,0 +1,9 @@
+export function AboutView() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is a dashboard for exploring exoplanets.</p>
+            <p>Include meta-information, like our GitHub repository link, description of team/goals, etc.</p>
+        </div>
+    );
+}
